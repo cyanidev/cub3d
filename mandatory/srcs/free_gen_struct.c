481d0761c2829ebbd8fee25e9a0dcc5d76554ead
Axel Caceres <acaceres@c2r8s5.42madrid.com>
2025-05-17 13:39:45 +0200

    refactor: mandatory/srcs/free_gen_struct.c

--- mandatory/srcs/free_gen_struct.c
+++ mandatory/srcs/free_gen_struct.c
@@ -6,7 +6,7 @@
 /*   By: Andie <Andie@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/08 19:41:08 by Andie             #+#    #+#             */
-/*   Updated: 2025/03/08 21:26:10 by Andie            ###   ########.fr       */
+/*   Updated: 2025/05/17 13:39:17 by acaceres         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,38 +17,26 @@
 
 void	free_gen_struct(t_cub *cub)
 {
-	if (cub)
-	{		
-		if (cub->mlx)
-			free(cub->mlx);
-		if (cub->north_path)
-			free(cub->north_path);
-		if (cub->south_path)
-			free(cub->south_path);
-		if (cub->east_path)
-			free(cub->east_path);
-		if (cub->west_path)
-			free(cub->west_path);
-
-		/*
-		
-		if (map)
-			afelicia_free_map();
-
-		*/
-		//if (cub->player)
-		//	;//
-
-		if (cub->north)
-			free_img(cub->north);
-		if (cub->south)
-			free_img(cub->south);
-		if (cub->east)
-			free_img(cub->east);
-		if (cub->west)
-			free_img(cub->west);
-
-		ft_bzero(cub, sizeof(t_cub));
-		free(cub);
-	}
+	if (!cub)
+		return ;
+	if (cub->mlx)
+		free(cub->mlx);
+	if (cub->north_path)
+		free(cub->north_path);
+	if (cub->south_path)
+		free(cub->south_path);
+	if (cub->east_path)
+		free(cub->east_path);
+	if (cub->west_path)
+		free(cub->west_path);
+	if (cub->north)
+		free_img(cub->north);
+	if (cub->south)
+		free_img(cub->south);
+	if (cub->east)
+		free_img(cub->east);
+	if (cub->west)
+		free_img(cub->west);
+	ft_bzero(cub, sizeof(t_cub));
+	free(cub);
 }
