c9d54427750b9d35f5f1dd7e4edfe12f84434e60
Axel Caceres <acaceres@c2r8s5.42madrid.com>
2025-05-20 16:47:34 +0200

    refactor: srcs/ray_casting.c

--- mandatory/includes/helper.h
+++ mandatory/includes/helper.h
@@ -6,12 +6,14 @@
 /*   By: acaceres <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/08 11:16:43 by acaceres          #+#    #+#             */
-/*   Updated: 2025/05/10 13:47:07 by acaceres         ###   ########.fr       */
+/*   Updated: 2025/05/20 16:46:28 by acaceres         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef HELPER_H
 # define HELPER_H
+
+# include <stddef.h>
 
 typedef struct s_helper
 {
@@ -21,5 +23,24 @@ typedef struct s_helper
 	float	y_prop;
 	float	aspect_ratio;
 }		t_helper;
+
+typedef struct s_rcast
+{
+	size_t	number_of_rays;
+	size_t	iterator;
+	float	start_angle;
+	float	multiplier;
+	float	max_dist;
+}		t_rcast;
+
+typedef struct s_dwall
+{
+	int		wall_height;
+	float	dist;
+	float	tmp2;
+	float	res_height;
+	double	last_time;
+}		t_dwall;
+
 
 #endif
--- mandatory/srcs/ray_casting.c
+++ mandatory/srcs/ray_casting.c
@@ -6,66 +6,55 @@
 /*   By: Andie <Andie@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/08 19:43:04 by Andie             #+#    #+#             */
-/*   Updated: 2025/03/08 19:43:05 by Andie            ###   ########.fr       */
+/*   Updated: 2025/05/20 16:45:30 by acaceres         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mlx.h"
 #include "cub.h"
 #include "idk.h"
+#include "helper.h"
 
-void	draw_walls_from_ray(float max_dist, size_t ray_n, float angle, t_cub *cub, t_cub_ray *ray)
+void	draw_walls_from_ray(t_rcast *r, t_cub *cub, t_cub_ray *ray)
 {
 	t_point	tmp;
-	int		wall_height;
-	float	dist;
-	float	tmp2;
-	float	res_height;
-	double	last_time;
+	t_dwall	dwall;
 
 	if (!ray)
 		return ;
-
-	res_height = (float)cub->game_img->resolution.height;
+	dwall.res_height = (float)cub->game_img->resolution.height;
 	tmp = cub->player->camera->pos;
-	dist = ray->real_dist;
-	dist = cub->player->camera->angle - angle;
-	dist = deg2_rad(dist);
-	dist = ray->real_dist * cos(dist);
-
-	tmp2 = cub->player->camera->pos.pz;
-	dist	= dist / tmp2;
-	dist = res_height / dist;
-	wall_height = (int)dist;
-
+	dwall.dist = ray->real_dist;
+	dwall.dist = cub->player->camera->angle - r->start_angle;
+	dwall.dist = deg2_rad(dwall.dist);
+	dwall.dist = ray->real_dist * cos(dwall.dist);
+	dwall.tmp2 = cub->player->camera->pos.pz;
+	dwall.dist = dwall.dist / dwall.tmp2;
+	dwall.dist = dwall.res_height / dwall.dist;
+	dwall.wall_height = (int)dwall.dist;
 	if (cub->game_mode == GAME)
-		draw_wall(max_dist, wall_height, cub, ray_n, ray, angle);
-
+		draw_wall(r->max_dist, dwall.wall_height,
+			cub, r->iterator, ray, r->start_angle);
 	ray->del(ray);
 }
 
-
 int	ray_casting(t_cub *cub, t_map_editor minimap)
 {
-	size_t	number_of_rays;
-	size_t	iterator;
-	float	start_angle;
-	float	multiplier;
-	float	max_dist;
+	t_rcast	r;
 
-	number_of_rays = cub->game_img->resolution.width;
-	multiplier = (float)cub->player->camera->fov / (float)number_of_rays;
-	start_angle = cub->player->camera->angle;
-	start_angle = start_angle - (cub->player->camera->fov / 2.0f);
-	iterator = 0;
-	max_dist = cub->max_dist;
-	while (iterator < number_of_rays)
+	r.number_of_rays = cub->game_img->resolution.width;
+	r.multiplier = cub->player->camera->fov / (float)r.number_of_rays;
+	r.start_angle = cub->player->camera->angle;
+	r.start_angle = r.start_angle - (cub->player->camera->fov / 2.0f);
+	r.iterator = 0;
+	r.max_dist = cub->max_dist;
+	while (r.iterator < r.number_of_rays)
 	{
-		draw_walls_from_ray(max_dist, iterator, start_angle, cub, cub_cast_ray(cub, fix_angle(start_angle), max_dist, minimap));
-
-		// angle
-		start_angle += multiplier;
-		// x
-		iterator++;
+		draw_walls_from_ray(&r, cub,
+			cub_cast_ray(cub,
+				fix_angle(r.start_angle),
+				r.max_dist, minimap));
+		r.start_angle += r.multiplier;
+		r.iterator++;
 	}
 }
