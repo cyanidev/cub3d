cefb23ea490bd4a9cb61aa3ed6d7d375a3ef8a1c
Axel Caceres <acaceres@c2r10s4.42madrid.com>
2025-05-22 17:33:13 +0200

    refactoring: mandatory/srcs/draw_wall.c

--- mandatory/srcs/draw_wall.c
+++ mandatory/srcs/draw_wall.c
@@ -6,7 +6,7 @@
 /*   By: Andie <Andie@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/08 19:40:30 by Andie             #+#    #+#             */
-/*   Updated: 2025/05/17 13:05:33 by acaceres         ###   ########.fr       */
+/*   Updated: 2025/05/22 17:31:16 by acaceres         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,36 +14,43 @@
 #include "cub.h"
 #include "idk.h"
 
-void draw_wall(float max_dist, int wall_height, t_cub *cub, size_t wall_n, t_cub_ray *ray, float angle)
+typedef struct s_drwall
 {
-	float	res_height;
-    int		wall_top;
-	int		min_top;
-    int		wall_bottom;
-	t_point	pixel;
+	int	wall_top;
+	int	min_top;
+	int	wall_bottom;
+	int	y;
+	int	real_pos;
+	int	mirror_y;
+	int	mirror_helper;
 	float	color_mix_lerp;
-	int		y;
-	int		real_pos;
 	float	real_pos_x;
-	int		mirror_y;
-	int		mirror_helper;
-	t_img	*texture;
+	float	res_height;
+}		t_drwall;
 
-	res_height = cub->game_img->resolution.height;
-	wall_top = (int)(res_height - (float)wall_height) / 2;
-	min_top = wall_top;
-	wall_bottom = wall_top + wall_height;
-    if (wall_top < 0)
-		wall_top = 0;
-    if (wall_bottom >= res_height) 
-		wall_bottom = (int)(res_height - 1.0f);
-	color_mix_lerp *= 4;
-	if (color_mix_lerp > 1)
-		color_mix_lerp = 1;
-	draw_sky_and_ground(cub, wall_top, wall_n, wall_bottom);
-	y = wall_top;
-	real_pos_x = get_real_pos_x(ray->real_x, ray->real_y, ray->side);
-	mirror_helper = 0;
+void	draw_wall(float max_dist, int wall_height, t_cub *cub, size_t wall_n, t_cub_ray *ray, float angle)
+{
+	t_point		pixel;
+	t_drwall	dw;
+	t_img		*texture;
+	t_color		tmp_color;
+	t_color		tmp_color2;
+
+	dw.res_height = cub->game_img->resolution.height;
+	dw.wall_top = (int)(dw.res_height - (float)wall_height) / 2;
+	dw.min_top = dw.wall_top;
+	dw.wall_bottom = dw.wall_top + wall_height;
+	if (dw.wall_top < 0)
+		dw.wall_top = 0;
+	if (dw.wall_bottom >= dw.res_height) 
+		dw.wall_bottom = (int)(dw.res_height - 1.0f);
+	dw.color_mix_lerp *= 4;
+	if (dw.color_mix_lerp > 1)
+		dw.color_mix_lerp = 1;
+	draw_sky_and_ground(cub, dw.wall_top, wall_n, dw.wall_bottom);
+	dw.y = dw.wall_top;
+	dw.real_pos_x = get_real_pos_x(ray->real_x, ray->real_y, ray->side);
+	dw.mirror_helper = 0;
 	texture = cub->test_tex;
 	if (ray->side == 2)
 		texture = cub->south;
@@ -54,35 +61,23 @@ void draw_wall(float max_dist, int wall_height, t_cub *cub, size_t wall_n, t_cub
 	if (ray->side == 3)
 		texture = cub->east;
 
-	while (y < wall_bottom)
+	while (dw.y < dw.wall_bottom)
 	{
-		real_pos = y - min_top;
+		dw.real_pos = dw.y - dw.min_top;
 		pixel.px = (float)wall_n;
-		pixel.py = (float)y;
+		pixel.py = (float)dw.y;
 		pixel.color	= color(WHITE);
-		pixel.color = color_from_hex(get_pixel_img(texture, real_pos_x * (float)texture->resolution.width, ((float)real_pos / (float)wall_height) * (float)texture->resolution.height));
-
+		pixel.color = color_from_hex(get_pixel_img(texture,
+				dw.real_pos_x
+					* (float)texture->resolution.width,
+				((float)dw.real_pos / (float)wall_height)
+					* (float)texture->resolution.height));
 		if (ray->side == 1 || ray->side == 3)
 			pixel.color = color_mix(pixel.color, color(BLACK), 0.75f);
-		pixel.color = color_mix(pixel.color, color(BLACK), color_mix_lerp);
-
-		t_color	tmp_color;
-		t_color	tmp_color2;
-		//tmp_color = color_from_hex(get_pixel_img(cub->test_tex, real_pos_x * (float)cub->test_tex->resolution.width, ((float)real_pos / (float)wall_height) * (float)cub->test_tex->resolution.height));
-	//	pixel.color = color_mix(pixel.color, tmp_color, 0.5f);
-
+		pixel.color = color_mix(pixel.color, color(BLACK), dw.color_mix_lerp);
 		tmp_color = pixel.color;
-
-
-	//	tmp_color2 = color_from_hex(get_pixel_img(cub->test_tex, real_pos_x * (float)cub->test_tex->resolution.width, ((float)real_pos / (float)wall_height) * (float)cub->test_tex->resolution.height));
-	//	pixel.color = color_mix(color(BLACK), tmp_color2, ((float)pixel.color.r / 255.0f));
-
 		put_pixel(cub->game_img, pixel);
-
-	//	tmp_color = pixel.color;
-		
-
-		y++;
-		mirror_helper++;
+		dw.y++;
+		dw.mirror_helper++;
 	}
 }
