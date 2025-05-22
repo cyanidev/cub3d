1dde3f50c9ae974ba413c13e980048a3c9e8cc47
Axel Caceres <acaceres@c2r10s4.42madrid.com>
2025-05-22 17:59:22 +0200

    refactoring: mandatory/srcs/draw_wall.c - created render wall

--- mandatory/srcs/draw_wall.c
+++ mandatory/srcs/draw_wall.c
@@ -6,7 +6,7 @@
 /*   By: Andie <Andie@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/08 19:40:30 by Andie             #+#    #+#             */
-/*   Updated: 2025/05/22 17:31:16 by acaceres         ###   ########.fr       */
+/*   Updated: 2025/05/22 17:58:07 by acaceres         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,67 @@
 
 typedef struct s_drwall
 {
-	int	wall_top;
-	int	min_top;
-	int	wall_bottom;
-	int	y;
-	int	real_pos;
-	int	mirror_y;
-	int	mirror_helper;
+	int		wall_top;
+	int		min_top;
+	int		wall_bottom;
+	int		y;
+	int		real_pos;
+	int		mirror_y;
+	int		mirror_helper;
 	float	color_mix_lerp;
 	float	real_pos_x;
 	float	res_height;
 }		t_drwall;
 
+typedef struct s_rwall
+{
+	t_cub		*cub;
+	t_cub_ray	*ray;
+	t_color		tmp_color;
+	float		wall_n;
+	float		wall_height;
+}		t_rwall;
+
+static void
+	render_wall(t_rwall *rwall, t_drwall *dw, t_point *pixel)
+{
+	t_img		*texture;
+
+	texture = rwall->cub->test_tex;
+	if (rwall->ray->side == 2)
+		texture = rwall->cub->south;
+	if (rwall->ray->side == 0)
+		texture = rwall->cub->north;
+	if (rwall->ray->side == 1)
+		texture = rwall->cub->west;
+	if (rwall->ray->side == 3)
+		texture = rwall->cub->east;
+	while (dw->y < dw->wall_bottom)
+	{
+		dw->real_pos = dw->y - dw->min_top;
+		pixel->px = rwall->wall_n;
+		pixel->py = (float)dw->y;
+		pixel->color	= color(WHITE);
+		pixel->color = color_from_hex(get_pixel_img(texture,
+				dw->real_pos_x
+					* (float)texture->resolution.width,
+				((float)dw->real_pos / rwall->wall_height)
+					* (float)texture->resolution.height));
+		if (rwall->ray->side == 1 || rwall->ray->side == 3)
+			pixel->color = color_mix(pixel->color, color(BLACK), 0.75f);
+		pixel->color = color_mix(pixel->color, color(BLACK), dw->color_mix_lerp);
+		rwall->tmp_color = pixel->color;
+		put_pixel(rwall->cub->game_img, *pixel);
+		dw->y++;
+		dw->mirror_helper++;
+	}
+}
+
 void	draw_wall(float max_dist, int wall_height, t_cub *cub, size_t wall_n, t_cub_ray *ray, float angle)
 {
 	t_point		pixel;
 	t_drwall	dw;
-	t_img		*texture;
-	t_color		tmp_color;
-	t_color		tmp_color2;
+	t_rwall		rwall;
 
 	dw.res_height = cub->game_img->resolution.height;
 	dw.wall_top = (int)(dw.res_height - (float)wall_height) / 2;
@@ -51,33 +93,9 @@ void	draw_wall(float max_dist, int wall_height, t_cub *cub, size_t wall_n, t_cub
 	dw.y = dw.wall_top;
 	dw.real_pos_x = get_real_pos_x(ray->real_x, ray->real_y, ray->side);
 	dw.mirror_helper = 0;
-	texture = cub->test_tex;
-	if (ray->side == 2)
-		texture = cub->south;
-	if (ray->side == 0)
-		texture = cub->north;
-	if (ray->side == 1)
-		texture = cub->west;
-	if (ray->side == 3)
-		texture = cub->east;
-
-	while (dw.y < dw.wall_bottom)
-	{
-		dw.real_pos = dw.y - dw.min_top;
-		pixel.px = (float)wall_n;
-		pixel.py = (float)dw.y;
-		pixel.color	= color(WHITE);
-		pixel.color = color_from_hex(get_pixel_img(texture,
-				dw.real_pos_x
-					* (float)texture->resolution.width,
-				((float)dw.real_pos / (float)wall_height)
-					* (float)texture->resolution.height));
-		if (ray->side == 1 || ray->side == 3)
-			pixel.color = color_mix(pixel.color, color(BLACK), 0.75f);
-		pixel.color = color_mix(pixel.color, color(BLACK), dw.color_mix_lerp);
-		tmp_color = pixel.color;
-		put_pixel(cub->game_img, pixel);
-		dw.y++;
-		dw.mirror_helper++;
-	}
+	rwall.cub = cub;
+	rwall.ray = ray;
+	rwall.wall_n = wall_n;
+	rwall.wall_height = (float)wall_height;
+	render_wall(&rwall, &dw, &pixel);
 }
